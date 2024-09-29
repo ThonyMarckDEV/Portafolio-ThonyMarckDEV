f38e9057ab698c65e7123b0ccf688a5f637447bb
ThonyMarckDEV <thonymarckyt08@gmail.com>
2024-09-28 23:01:26 -0500

    Updates

---
+++ precache-manifest.ccd937243ab1868c9f57179e3f14c897.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc36f7be717d6da53dfa859b0356678",
+    "url": "/Portafolio-ThonyMarckDEV/index.html"
+  },
+  {
+    "revision": "4189baaecf2c4b4c064a",
+    "url": "/Portafolio-ThonyMarckDEV/static/css/main.54f455e1.chunk.css"
+  },
+  {
+    "revision": "dc2f52158a04bb3c4eb9",
+    "url": "/Portafolio-ThonyMarckDEV/static/js/2.96ba2204.chunk.js"
+  },
+  {
+    "revision": "4189baaecf2c4b4c064a",
+    "url": "/Portafolio-ThonyMarckDEV/static/js/main.5cc6f2de.chunk.js"
+  },
+  {
+    "revision": "2a34b86b3b6b20c8455a",
+    "url": "/Portafolio-ThonyMarckDEV/static/js/runtime~main.debaa713.js"
+  }
+]);
