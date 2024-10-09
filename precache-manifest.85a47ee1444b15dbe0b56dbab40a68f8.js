f6f6eaf173f6b5b5e5448c49f86f2dac49e8d87c
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2024-10-09 12:51:18 -0500

    Updates

--- precache-manifest.85a47ee1444b15dbe0b56dbab40a68f8.js
+++ precache-manifest.85a47ee1444b15dbe0b56dbab40a68f8.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cd8efea1a5eeff3289b4981ed63b2fc7",
+    "revision": "a6c0fd23d4050fd77fd48309bd214804",
     "url": "/Portafolio-ThonyMarckDEV/index.html"
   },
   {
-    "revision": "6d577924c0bdcc9e51fc",
-    "url": "/Portafolio-ThonyMarckDEV/static/css/main.950d917d.chunk.css"
+    "revision": "dc20c20b31fef7c823ac",
+    "url": "/Portafolio-ThonyMarckDEV/static/css/main.a82327b0.chunk.css"
   },
   {
     "revision": "92e23421ff612ea251ce",
     "url": "/Portafolio-ThonyMarckDEV/static/js/2.8851f76e.chunk.js"
   },
   {
-    "revision": "6d577924c0bdcc9e51fc",
+    "revision": "dc20c20b31fef7c823ac",
     "url": "/Portafolio-ThonyMarckDEV/static/js/main.7aeaebed.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portafolio-ThonyMarckDEV/precache-manifest.1464d2918fe71ff64c3fbe9de8a920fe.js"
+  "/Portafolio-ThonyMarckDEV/precache-manifest.85a47ee1444b15dbe0b56dbab40a68f8.js"
 );
 
 self.addEventListener('message', (event) => {
